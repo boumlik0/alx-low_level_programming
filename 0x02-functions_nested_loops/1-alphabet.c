c40e10c13f1c159303f95e4ea1a15b7fa38c624a
boumlik0 <boumlik4130@gmail.com>
2023-03-21 13:26:40 +0000

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -14,10 +14,10 @@ void print_alphabet(void) {
     char c = 'a';
 
     while (c <= 'z') {
-        putchar(c);
+        _putchar(c);
         c++;
     }
 
-    putchar('\n');
+    _putchar('\n');
 
 }
