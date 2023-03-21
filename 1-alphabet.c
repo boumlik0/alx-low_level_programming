de15ac4f13d77cdc955e6b3a56ee283e70d9dee8
boumlik0 <boumlik4130@gmail.com>
2023-03-21 13:24:43 +0000

    1-alphabet.c

---
+++ 1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+void print_alphabet(void);
+
+    int main() {
+        print_alphabet();
+        return 0;
+    }
+
+void print_alphabet(void) {
+
+    char c = 'a';
+
+    while (c <= 'z') {
+        _putchar(c);
+        c++;
+    }
+
+    _putchar('\n');
+
+}
